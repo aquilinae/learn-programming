b71341f70906a354c98fb393ed50bb9be05e96d0
Nikolai <ornichola@users.noreply.github.com>
2017-10-25 17:29:52 +0300

    Adding missing brackets

--- learn-web/books/EloquentJavascript/04_02.js
+++ learn-web/books/EloquentJavascript/04_02.js
@@ -13,19 +13,20 @@ FizzBuzz
 решите эту задачу, можете себя похвалить.)
 */
 
-for (var number = 1; number <= 100; number++)
+for (var number = 1; number <= 100; number++) {
   if (number % 3 == 0) console.log('Fizz');
   else if (number % 5 == 0 && number % 3 != 0) console.log('Buzz');
   else console.log(number);
-
+}
 
 /* ... исправьте её так, чтобы она выводила
 «FizzBuzz» для всех чисел, которые делятся и на 3, и на
 5.
 */
 
-for (var number = 1; number <= 100; number++)
+for (var number = 1; number <= 100; number++) {
   if (number % 3 == 0 && number % 5 == 0) console.log('FizzBuzz');
   else if (number % 3 == 0) console.log('Fizz');
   else if (number % 5 == 0 && number % 3 != 0) console.log('Buzz');
   else console.log(number);
+}
