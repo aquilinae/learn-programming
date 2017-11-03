f32f2b4f6e5eabd2d7fa3c982304e71526a0897e
Nikolai <ornichola@users.noreply.github.com>
2017-11-03 14:23:14 +0300

    ch04 ex03 task03

--- learn-web/books/EloquentJavascript/04_03.js
+++ learn-web/books/EloquentJavascript/04_03.js
@@ -69,3 +69,14 @@ function listToArray(list) {
 
 console.log(listToArray(arrayToList([10, 20, 30])));
 // → [10, 20, 30]
+
+function prepend(value, rest) {
+  var list = {
+    value: value,
+    rest: rest
+  }
+  return list;
+}
+
+console.log(prepend(10, prepend(20, null)));
+// → {value: 10, rest: {value: 20, rest: null}}
