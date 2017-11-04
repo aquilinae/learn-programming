9174acb87f28ab8ba7e02f585c1828c4c7375722
ornichola <1502708+ornichola@users.noreply.github.com>
2017-11-04 23:33:18 +0300

    moving result to the SecondActivity

--- learn-android/TechColl/SimpleCalc/app/src/main/java/com/example/ornichola/simplecalc/MainActivity.java
+++ learn-android/TechColl/SimpleCalc/app/src/main/java/com/example/ornichola/simplecalc/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.ornichola.simplecalc;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -21,7 +22,6 @@ public class MainActivity extends AppCompatActivity {
 
         value01 = (EditText)findViewById(R.id.value01);
         value02 = (EditText)findViewById(R.id.value02);
-        result = (TextView)findViewById(R.id.result);
         add = (Button)findViewById(R.id.buttonAdd);
         subtract = (Button)findViewById(R.id.buttonSubtract);
         multiply = (Button)findViewById(R.id.buttonMultiply);
@@ -34,7 +34,10 @@ public class MainActivity extends AppCompatActivity {
                     double v1 = Double.parseDouble(value01.getText().toString());
                     double v2 = Double.parseDouble(value02.getText().toString());
                     double res = v1 + v2;
-                    result.setText(String.valueOf(res));
+
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("RESULT", res);
+                    startActivity(intent);
                 }
                 else {
                     Toast.makeText(v.getContext(),"Введите оба числа!",Toast.LENGTH_SHORT).show();
@@ -49,7 +52,10 @@ public class MainActivity extends AppCompatActivity {
                     double v1 = Double.parseDouble(value01.getText().toString());
                     double v2 = Double.parseDouble(value02.getText().toString());
                     double res = v1 - v2;
-                    result.setText(String.valueOf(res));
+
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("RESULT", res);
+                    startActivity(intent);
                 }
                 else {
                     Toast.makeText(v.getContext(),"Введите оба числа!",Toast.LENGTH_SHORT).show();
@@ -64,7 +70,10 @@ public class MainActivity extends AppCompatActivity {
                     double v1 = Double.parseDouble(value01.getText().toString());
                     double v2 = Double.parseDouble(value02.getText().toString());
                     double res = v1 * v2;
-                    result.setText(String.valueOf(res));
+
+                    Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                    intent.putExtra("RESULT", res);
+                    startActivity(intent);
                 }
                 else {
                     Toast.makeText(v.getContext(),"Введите оба числа!",Toast.LENGTH_SHORT).show();
@@ -83,7 +92,10 @@ public class MainActivity extends AppCompatActivity {
                     }
                     else {
                         double res = v1 / v2;
-                        result.setText(String.valueOf(res));
+
+                        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                        intent.putExtra("RESULT", res);
+                        startActivity(intent);
                     }
                 }
                 else {
---
+++ learn-android/TechColl/SimpleCalc/app/src/main/java/com/example/ornichola/simplecalc/SecondActivity.java
@@ -0,0 +1,22 @@
+package com.example.ornichola.simplecalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    TextView result;
+    double res;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        res = getIntent().getExtras().getDouble("RESULT");
+
+        result = (TextView)findViewById(R.id.result);
+        result.setText(String.valueOf(res));
+    }
+}
