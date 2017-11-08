24b917ce525ff66c0f016c73062080383a88ac5c
Nikolai <ornichola@users.noreply.github.com>
2017-11-08 15:16:17 +0300

    ch05 ex04 every() elegant-implementation

--- learn-web/books/EloquentJavascript/05_04.js
+++ learn-web/books/EloquentJavascript/05_04.js
@@ -49,6 +49,14 @@ function some(array, condition) {
   return false;
 }
 
+function every(array, condition) {
+  for (var i = 0; i < array.length; i++) {
+    if (!condition(array[i]))
+      return false;
+  }
+  return true;
+}
+
 console.log(every([NaN, NaN, NaN], isNaN));
 // → true
 console.log(every([NaN, NaN, 4], isNaN));
