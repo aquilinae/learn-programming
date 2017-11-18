cdda20e992bb97d6ade112b0beb40f0ce7c08879
ornichola <1502708+ornichola@users.noreply.github.com>
2017-11-18 21:59:32 +0300

    PythonCrashCourse ch18 ex05

---
+++ learn-python/books/PythonCrashCourse/ch18/18-05/meal_planner/meal_plans/models.py
@@ -0,0 +1,5 @@
+from django.db import models
+
+# Create your models here.
+class Meal(models.Model):
+    pass
---
+++ learn-python/books/PythonCrashCourse/ch18/18-05/meal_planner/virt-env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ornichola/GitHub/learn-programming/learn-python/books/PythonCrashCourse/ch18/18-05/meal_planner/virt-env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
