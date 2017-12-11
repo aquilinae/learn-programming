f70c3940f2f39bf4e7d2a8d580e3a9d4805e29f6
ornichola <1502708+ornichola@users.noreply.github.com>
2017-12-11 21:26:58 +0300

    removing non-related python exercises

--- learn-python/books/PythonCrashCourse/ch18/ex01/FaceGram/virt-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ornichola/GitHub/learn-programming/learn-python/books/PythonCrashCourse/ch18/FaceGram/virt-env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- learn-python/djangogirls/virt-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/ornichola/GitHub/learn-programming/learn-python/djangogirls/virt-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
